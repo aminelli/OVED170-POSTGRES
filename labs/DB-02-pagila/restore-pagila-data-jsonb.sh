b92a25a25d85c33dc89fe6878e35296bff02b416
aminelli <56305052+aminelli@users.noreply.github.com>
2025-06-06 08:39:22 +0200

    upd

--- labs/DB-02-pagila/restore-pagila-data-jsonb.sh
+++ labs/DB-02-pagila/restore-pagila-data-jsonb.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "IMPORT"
+
 pg_restore /docker-entrypoint-initdb.d/pagila-data-apt-jsonb.backup -d postgres
 
 pg_restore /docker-entrypoint-initdb.d/pagila-data-yum-jsonb.backup -d postgres
